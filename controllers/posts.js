26e4fd756195344f973c0e23f19515d68b54dad3
owaisghaira <owais.ghaira99@gmail.com>
2021-01-03 19:00:51 +0500

    create post method added

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,24 @@
-export const setPosts =  (req, res) => {
-    res.send('works...')
+import PostMessge from '../models/postMessage.js';
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessge.find();
+        res.status(200).json(postMessages)
+    } catch (error) {
+        res.status(404).json({ msg: error.message })
+    }
+}
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+    const newPost = new PostMessge(post)
+
+    try {
+
+        await newPost.save();
+        res.status(201).json(newPost)
+
+    } catch (error) {
+        res.status(409).json({ msg: error.message })
+    }
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,10 @@
 import express from 'express'
 
-import { setPosts } from '../controllers/posts.js'
+import { getPosts,createPost } from '../controllers/posts.js'
 
 const router = express.Router();
 
-router.get('/', setPosts)
+router.get('/', getPosts)
+router.post('/', createPost)
 
 export default router;
