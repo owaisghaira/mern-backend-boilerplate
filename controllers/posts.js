5d6c4d063ca1a306688ed4c97ee12cd4284006a9
owaisghaira <owais.ghaira99@gmail.com>
2021-01-05 23:44:00 +0500

    create update & delete routes added

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,5 @@
 import PostMessge from '../models/postMessage.js';
+import mongoose from 'mongoose'
 
 export const getPosts = async (req, res) => {
     try {
@@ -21,4 +22,25 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({ msg: error.message })
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send(`No post with that Id`);
+
+    const updatedpost = await PostMessge.findByIdAndUpdate(_id, post, { new: true })
+
+    res.json(updatedpost)
+}
+
+export const deletePost = async (req, res) => {
+    const { id: _id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send(`No post with that Id`);
+
+    await PostMessge.findByIdAndRemove(_id)
+
+    res.json({message:'post delete succesfully'})
 }
